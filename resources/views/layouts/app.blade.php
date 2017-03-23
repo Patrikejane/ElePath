ab53b5d7dddc58864eac501675d3196e046db9b5
patrikejane <sskmal@gmail.com>
2017-03-23 14:28:22 +0530

    createUser through navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,6 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -66,6 +65,9 @@
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
+                    @if (Auth::user()->admin ==1)
+                        <li><a href="{{ url('/createuser') }}">Register</a></li>
+                    @endif
                     @endif
                 </ul>
             </div>
